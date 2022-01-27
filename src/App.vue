0e0b187090dfb2a009c7182f879e574584cba807
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-01-27 16:38:39 +0100

    added images and created header

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
   <div id="app">
-    
+    <header-box />
   </div>
 </template>
 
 <script>
+import HeaderBox from './components/HeaderBox.vue'
 
 export default {
   name: 'App',
   components: {
-
+    HeaderBox,
   }
 }
 </script>
 
 <style lang="scss">
-
+@import './style/main.scss';
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
 </style>
---
+++ src/components/HeaderBox.vue
@@ -0,0 +1,68 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="img-box">
+                <img src="@/img/dc-logo.png" alt="">
+            </div>
+            <div class="nav-links">
+                <ul>
+                    <li>characters</li>
+                    <li>comics</li>
+                    <li>movies</li>
+                    <li>tv</li>
+                    <li>games</li>
+                    <li>collectibles</li>
+                    <li>videos</li>
+                    <li>fans</li>
+                    <li>news</li>
+                    <li>shop</li>
+                </ul>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/style/variables.scss';
+
+header {
+    color: $text-gray;
+    background-color: white;
+    
+
+    .container {
+        width: 1200px;
+        margin: 0 auto;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
+    
+    ul {
+        text-transform: uppercase;
+        display: flex;
+
+        li {
+            font-size: 15px;
+            font-weight: 700;
+            padding: 70px 0;
+            margin: 0 10px;
+            border-bottom: 7px transparent solid;
+            cursor: pointer;
+
+            &:hover {
+                color: $text-blue;
+                border-bottom: 7px $text-blue solid;
+            }
+        }
+    }
+
+}
+
+</style>
