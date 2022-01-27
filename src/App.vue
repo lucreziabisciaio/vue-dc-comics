b8332e9e2b0968ae4d6678c6a8521c077d3b8bce
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-01-27 16:49:53 +0100

    created Main content

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <header-box />
+    <main-box />
   </div>
 </template>
 
 <script>
-import HeaderBox from './components/HeaderBox.vue'
+import HeaderBox from './components/HeaderBox.vue';
+import MainBox from './components/MainBox.vue';
 
 export default {
   name: 'App',
   components: {
     HeaderBox,
+    MainBox,
   }
 }
 </script>
---
+++ src/components/MainBox.vue
@@ -0,0 +1,33 @@
+<template>
+    <main>
+       <div class="container">
+           <h1>-- Content goes here --</h1>
+       </div>
+    </main> 
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@/style/variables.scss';
+
+main {
+    background-color: $text-gray;
+
+    .container {
+        width: 1200px;
+        margin: 0 auto;
+
+        h1 {
+            color: white;
+            font-size: 50px;
+            padding: 100px 0;
+        }
+    }
+}
+
+</style>
