0d803e18b729ddd01b1f7d0b19c43c2eaa9b38a2
lucreziabisciaio <lucreziabisciaio@gmail.com>
2022-01-28 16:58:17 +0100

    added comics card

--- src/components/MainBox.vue
+++ src/components/MainBox.vue
@@ -1,14 +1,26 @@
 <template>
     <main>
        <div class="container">
-           <h1>-- Content goes here --</h1>
+           <div class="card-box">
+               <div class="card" v-for="(card, index) in cardList" :key="index">
+                    <div class="card-img">
+                        <img :src="card.thumb" alt="">
+                    </div>
+                    
+                    <div class="card-title">
+                        <p>{{ card.series }}</p>
+                    </div>
+               </div>
+           </div>
        </div>
     </main> 
 </template>
 
 <script>
 export default {
-    
+    props: {
+        cardList: Array,
+    }
 }
 </script>
 
@@ -18,12 +30,36 @@ export default {
 main {
     background-color: #1C1C1C;
 
-    .container {
+    .card-box {
+        width: 100%;
+        padding: 30px 0;
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
         
-        h1 {
-            color: white;
-            font-size: 30px;
-            padding: 100px 0;
+        .card {
+            width: calc((100% / 6) - 20px);
+            padding: 0 10px;
+            margin: 15px 0;
+
+            .card-img {
+                width: 100%;
+                height: 150px;
+                overflow: hidden;
+
+                img {
+                    width: 100%;
+                }  
+            }
+
+            p {
+                color: white;
+                text-transform: uppercase;
+                font-weight: 700;
+                font-size: 15px;
+                padding: 5px 0;
+            }
+            
         }
     }
 }
